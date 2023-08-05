690a622049882280f4a9d8dbf8ce3a7da713b97f
Ebube Glory Ogbonda <ogbondaglory@gmail.com>
2023-08-05 22:03:56 +0100

    Update DeformableBsplineRegistration.py

--- DeformableBsplineRegistration.py
+++ DeformableBsplineRegistration.py
@@ -8,8 +8,8 @@ from utils import *
 # DeformableBsplineRegistration.py
 
 def perform_deformable_bspline_registration(fixed_image, moving_image, output_path, final_transform_v1, resampled_moving_image):
-    # print("Fixed Image Pixel Values - Min:", sitk.GetArrayViewFromImage(fixed_image).min(), "Max:", sitk.GetArrayViewFromImage(fixed_image).max())
-    # print("Moving Image Pixel Values - Min:", sitk.GetArrayViewFromImage(moving_image).min(), "Max:", sitk.GetArrayViewFromImage(moving_image).max())
+    print("Fixed Image Pixel Values - Min:", sitk.GetArrayViewFromImage(fixed_image).min(), "Max:", sitk.GetArrayViewFromImage(fixed_image).max())
+    print("Moving Image Pixel Values - Min:", sitk.GetArrayViewFromImage(moving_image).min(), "Max:", sitk.GetArrayViewFromImage(moving_image).max())
 
     if os.path.exists(output_path + "/bspline_deformable_transformation.tfm") and os.path.exists(output_path + "/composite_transform.tfm") and os.path.exists(output_path + "/bspline_deformable_registration.mha"):
         final_deformable_transform = sitk.ReadTransform(output_path + "/bspline_deformable_transformation.mha")
@@ -34,7 +34,7 @@ def perform_deformable_bspline_registration(fixed_image, moving_image, output_pa
         transform_domain_mesh_size = [size//4 for size in transform_domain_physical_dim_size]
         initial_deformable_transform = sitk.BSplineTransformInitializer(image1=fixed_image,
                                                                         transformDomainMeshSize=transform_domain_mesh_size, order=3)
-        print("Initial Transformation Parameters: ", initial_deformable_transform.GetParameters())
+        # print("Initial Transformation Parameters: ", initial_deformable_transform.GetParameters())
 
         # Use a multi-resolution strategy
         deformable_registration_method.SetShrinkFactorsPerLevel(shrinkFactors=[4, 2, 1])
@@ -50,7 +50,7 @@ def perform_deformable_bspline_registration(fixed_image, moving_image, output_pa
 
         final_deformable_transform = deformable_registration_method.Execute(sitk.Cast(fixed_image, sitk.sitkFloat32),
                                                                             sitk.Cast(resampled_moving_image, sitk.sitkFloat32))
-        print("Final Transformation Parameters: ", final_deformable_transform.GetParameters())
+        # print("Final Transformation Parameters: ", final_deformable_transform.GetParameters())
 
         # Combine the affine and deformable transforms
         composite_transform = sitk.CompositeTransform(fixed_image.GetDimension())
