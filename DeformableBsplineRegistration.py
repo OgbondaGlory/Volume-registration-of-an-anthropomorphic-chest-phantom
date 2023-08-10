decc37e73b4f519ac5d3672877e5456d827a9b62
Ebube Glory Ogbonda <ogbondaglory@gmail.com>
2023-08-11 00:49:06 +0100

    Refactored code
    Include :
    exception handling,
    commented image normalisation
    added registration organ mask, lungs and bones from the segmentation results

--- DeformableBsplineRegistration.py
+++ DeformableBsplineRegistration.py
@@ -54,7 +54,7 @@ def setup_deformable_registration(fixed_image):
 
     return deformable_registration_method
 
-def perform_deformable_bspline_registration(fixed_image, moving_image, output_path, final_transform_v1, resampled_moving_image):
+def perform_deformable_bspline_registration(fixed_image, moving_image, output_path, final_transform_v1, resampled_moving_image, mask_name):
     """Perform deformable B-spline registration.
     
     Args:
@@ -63,70 +63,77 @@ def perform_deformable_bspline_registration(fixed_image, moving_image, output_pa
         output_path (str): Path to save the registration results.
         final_transform_v1 (SimpleITK.Transform): Initial transformation (likely from previous registration steps).
         resampled_moving_image (SimpleITK.Image): Moving image after initial registration.
+        mask_name (str): Name of the mask for naming the output files.
 
     Returns:
         tuple: Tuple containing the final deformable transform and the resampled moving image.
     """
-
-    # Normalize the fixed and moving images before registration
-    fixed_image = normalize_image(fixed_image)
-    moving_image = normalize_image(moving_image)
-
-    # Convert the SimpleITK Image to a numpy array
-    fixed_image_array = sitk.GetArrayViewFromImage(fixed_image)
-    moving_image_array = sitk.GetArrayViewFromImage(moving_image)
-
-    # Display the min and max pixel values for the fixed and moving images
-    print("Fixed Image Pixel Values - Min:", fixed_image_array.min(), "Max:", fixed_image_array.max())
-    print("Moving Image Pixel Values - Min:", moving_image_array.min(), "Max:", moving_image_array.max())
-
-    # Check if previous results exist to avoid redundant computation
-    if os.path.exists(output_path + "/bspline_deformable_transformation.tfm") and \
-       os.path.exists(output_path + "/composite_transform.tfm") and \
-       os.path.exists(output_path + "/bspline_deformable_registration.mha"):
+    try:
+        # Normalize the fixed and moving images before registration
+        # fixed_image = normalize_image(fixed_image)
+        # moving_image = normalize_image(moving_image)
+    
+        # Convert the SimpleITK Image to a numpy array for statistics
+        fixed_image_array = sitk.GetArrayViewFromImage(fixed_image)
+        moving_image_array = sitk.GetArrayViewFromImage(moving_image)
         
-        # Read previous results from disk
-        final_deformable_transform = sitk.ReadTransform(output_path + "/bspline_deformable_transformation.mha")
-        composite_transform = sitk.ReadTransform(output_path + "/composite_transform.mha")
-        resampled_moving_image_deformable = sitk.ReadImage(output_path + "/bspline_deformable_registration.mha")
-    else:
-        # Setup the deformable registration method
-        deformable_registration_method = setup_deformable_registration(fixed_image)
-
-        # Initialize the B-spline transform with given parameters
-        transform_domain_physical_dim_size = fixed_image.GetSize()
-        transform_domain_mesh_size = [size // 4 for size in transform_domain_physical_dim_size]
-        initial_deformable_transform = sitk.BSplineTransformInitializer(image1=fixed_image,
-                                                                        transformDomainMeshSize=transform_domain_mesh_size, order=3)
-
-        # Set the initial transform for the registration process
-        deformable_registration_method.SetInitialTransform(initial_deformable_transform)
+        # Display the min and max pixel values for the fixed and moving images
+        print("Fixed Image Pixel Values - Min:", fixed_image_array.min(), "Max:", fixed_image_array.max())
+        print("Moving Image Pixel Values - Min:", moving_image_array.min(), "Max:", moving_image_array.max())
         
-        # Add a command to be called at every iteration for logging or updating UI
-        deformable_registration_method.AddCommand(sitk.sitkIterationEvent, lambda: iteration_callback(deformable_registration_method))
+        # Construct the file paths for saving the results
+        bspline_transform_path = os.path.join(output_path, f"bspline_deformable_transformation_{mask_name}.tfm")
+        composite_transform_path = os.path.join(output_path, f"composite_transform_{mask_name}.tfm")
+        registration_image_path = os.path.join(output_path, f"bspline_deformable_registration_{mask_name}.mha")
 
-        # Execute the registration method
-        final_deformable_transform = deformable_registration_method.Execute(sitk.Cast(fixed_image, sitk.sitkFloat32),
-                                                                            sitk.Cast(resampled_moving_image, sitk.sitkFloat32))
+        # If results exist on disk, load them
+        if os.path.exists(bspline_transform_path) and \
+        os.path.exists(composite_transform_path) and \
+        os.path.exists(registration_image_path):
+            
+            final_deformable_transform = sitk.ReadTransform(bspline_transform_path)
+            composite_transform = sitk.ReadTransform(composite_transform_path)
+            resampled_moving_image_deformable = sitk.ReadImage(registration_image_path)
 
-        # Combine the initial transformation and the computed deformable transformation
-        composite_transform = sitk.CompositeTransform(fixed_image.GetDimension())
-        composite_transform.AddTransform(final_transform_v1)
-        composite_transform.AddTransform(final_deformable_transform)
+        else:
+            # Setup the deformable registration method
+            deformable_registration_method = setup_deformable_registration(fixed_image)
+            
+            # Initialize the B-spline transform with given parameters
+            transform_domain_physical_dim_size = fixed_image.GetSize()
+            transform_domain_mesh_size = [size // 4 for size in transform_domain_physical_dim_size]
+            initial_deformable_transform = sitk.BSplineTransformInitializer(fixed_image, transform_domain_mesh_size, order=3)
+            
+            deformable_registration_method.SetInitialTransform(initial_deformable_transform)
 
-        # Save the transform to disk
-        sitk.WriteTransform(final_deformable_transform, output_path + "/bspline_deformable_transformation.tfm")
+            # Add a command to be called at every iteration for logging or updating UI
+            # Commenting out the iteration callback
+            # deformable_registration_method.AddCommand(sitk.sitkIterationEvent, lambda: iteration_callback(deformable_registration_method))
 
-        # Resample the moving image using the composite transformation
-        resampler = sitk.ResampleImageFilter()
-        resampler.SetReferenceImage(fixed_image)
-        resampler.SetTransform(composite_transform)
-        resampled_moving_image_deformable = resampler.Execute(resampled_moving_image)
+            # Execute the registration
+            final_deformable_transform = deformable_registration_method.Execute(sitk.Cast(fixed_image, sitk.sitkFloat32), sitk.Cast(resampled_moving_image, sitk.sitkFloat32))
+            
+            # Combine the initial and deformable transformations
+            composite_transform = sitk.CompositeTransform(fixed_image.GetDimension())
+            composite_transform.AddTransform(final_transform_v1)
+            composite_transform.AddTransform(final_deformable_transform)
 
-        # Save the resampled moving image to disk
-        sitk.WriteImage(resampled_moving_image_deformable, output_path + "/bspline_deformable_registration.mha")
+            # Save the transform to disk
+            sitk.WriteTransform(final_deformable_transform, bspline_transform_path)
+            
+            # Resample the moving image
+            resampler = sitk.ResampleImageFilter()
+            resampler.SetReferenceImage(fixed_image)
+            resampler.SetTransform(composite_transform)
+            resampled_moving_image_deformable = resampler.Execute(resampled_moving_image)
 
-    # Display the min and max pixel values for the resampled moving image
-    print("Resampled Moving Image Pixel Values - Min:", sitk.GetArrayViewFromImage(resampled_moving_image_deformable).min(), "Max:", sitk.GetArrayViewFromImage(resampled_moving_image_deformable).max())
+            # Save the resampled moving image
+            sitk.WriteImage(resampled_moving_image_deformable, registration_image_path)
+        
+        print("Resampled Moving Image Pixel Values - Min:", sitk.GetArrayViewFromImage(resampled_moving_image_deformable).min(), "Max:", sitk.GetArrayViewFromImage(resampled_moving_image_deformable).max())
 
-    return final_deformable_transform, resampled_moving_image_deformable
+        return final_deformable_transform, resampled_moving_image_deformable
+ 
+    except Exception as e:
+            print(f"An error occurred during registration: {e}")
+            return None, None
--- Deformable_Image_Registration.py
+++ Deformable_Image_Registration.py
@@ -16,141 +16,142 @@ from TrainingDNN import train_dnn_model
 def Deformable_Image_Registration(patient_image_path, phantom_image_path, output_path, operation):
 
     # print(0)
-    print(patient_image_path, phantom_image_path, output_path, operation)
-    
     # Load the DICOM images for both patient and phantom
     patient_ct_image = load_dicom_series(patient_image_path)
     phantom_ct_image = load_dicom_series(phantom_image_path)
 
-    # Perform lung segmentation
-    if operation == 'segment':
-        # Segment lung on both patient and phantom CT images
-        # print(1)
-        patient_lung_mask = segment_lung(patient_ct_image)
-        # print(2)
-        phantom_lung_mask = segment_lung(phantom_ct_image)
-        # print(3)
+    # Paths for segmentation masks
+    patient_lung_mask_path = os.path.join(output_path, os.path.basename(patient_image_path) + "_lung_mask.mha")
+    phantom_lung_mask_path = os.path.join(output_path, "Phantom_CT_Scan_lung_mask.mha")
 
-        # Save lung segmentation results
-        save_segmentation(patient_lung_mask, patient_image_path, output_path, "lung")
-        # print(4)
-        save_segmentation(phantom_lung_mask, phantom_image_path, output_path, "lung")
-        # print(5)
-
-        # Perform bone segmentation
-        patient_bone_mask = segment_bones(patient_ct_image)
-        # print(6)
-        phantom_bone_mask = segment_bones(phantom_ct_image)
-        # print(7)
-
-        # Save bone segmentation results
-        save_segmentation(patient_bone_mask, patient_image_path, output_path, "bone")
-        # print(8)
-        save_segmentation(phantom_bone_mask, phantom_image_path, output_path, "bone")
-        # print(9)
-
+    # Load or generate lung segmentation masks
+    if os.path.exists(patient_lung_mask_path) and os.path.exists(phantom_lung_mask_path):
+        # Load masks from disk
+        patient_lung_mask = sitk.ReadImage(patient_lung_mask_path)
+        phantom_lung_mask = sitk.ReadImage(phantom_lung_mask_path)
     else:
-        # Load the DICOM images
-        fixed_image = load_dicom_series(patient_image_path)
-        moving_image = load_dicom_series(phantom_image_path)
+        if operation == 'segment':
+            # Segment lung on both patient and phantom CT images
+            patient_lung_mask = segment_lung(patient_ct_image)
+            phantom_lung_mask = segment_lung(phantom_ct_image)
 
-        writer = sitk.ImageFileWriter()
+            # Save lung segmentation results
+            save_segmentation(patient_lung_mask, patient_image_path, output_path, "lung")
+            save_segmentation(phantom_lung_mask, phantom_image_path, output_path, "lung")
 
-        if operation == 'rigid':
-            print("Performing rigid registration...")
-            final_transform_v1, resampled_moving_image = perform_rigid_registration(fixed_image, moving_image, output_path)
-            print("Rigid registration completed.")
-            # Generate checkerboard for Rigid Registration
-            checker_image = generate_checkerboard(fixed_image, resampled_moving_image)
-            save_images(checker_image, output_path, "checkerboard_rigid_registration")
-            # Display the checkerboard image for Rigid Registration
-            display_images(checker_image, "Checkerboard for Rigid Registration")
+            # Perform bone segmentation
+            patient_bone_mask = segment_bones(patient_ct_image)
+            phantom_bone_mask = segment_bones(phantom_ct_image)
 
-            # Extracting the ISO Surfaces for Rigid Registration
-            output_resampled_image_path = os.path.join(output_path, "rigid_registration.mha")
-            output_iso_surface_file_path = os.path.join(output_path, "iso_surface_rigid.stl")
-            resampled_moving_image = sitk.ReadImage(output_resampled_image_path)
-            verts, faces = extract_iso_surface(resampled_moving_image, level=0.5, smooth=0.0)
-            save_iso_surface(verts, faces, output_iso_surface_file_path)
+            # Save bone segmentation results
+            save_segmentation(patient_bone_mask, patient_image_path, output_path, "bone")
+            save_segmentation(phantom_bone_mask, phantom_image_path, output_path, "bone")
+    # At this point, you have patient_lung_mask and phantom_lung_mask ready for use, whether they were loaded or segmented just now.
+    if operation != 'segment':
+    # Define masks for the two registration pipelines
+        fixed_images = [patient_lung_mask, patient_bone_mask]
+        moving_images = [phantom_lung_mask, phantom_bone_mask]
+        mask_names = ['lung', 'bone']
 
-        elif operation == 'bspline':
-            print("Performing deformable B-spline registration...")
-            # Check if final_transform_v1 and resampled_moving_image from rigid operation are available
-            rigid_transform_file = os.path.join(output_path, "rigid_transformation.tfm")
-            resampled_moving_image_file = os.path.join(output_path, "rigid_registration.mha")
-            if os.path.exists(rigid_transform_file) and os.path.exists(resampled_moving_image_file):
-                print("Loading results from previous rigid registration...")
-                final_transform_v1 = sitk.ReadTransform(rigid_transform_file)
-                resampled_moving_image = sitk.ReadImage(resampled_moving_image_file)
-            else:
-                print("Performing preliminary rigid registration...")
+        for idx, (fixed_image, moving_image, mask_name) in enumerate(zip(fixed_images, moving_images, mask_names)):
+        # writer = sitk.ImageFileWriter()
+
+            if operation == 'rigid':
+                print("Performing rigid registration...")
                 final_transform_v1, resampled_moving_image = perform_rigid_registration(fixed_image, moving_image, output_path)
-                print("Preliminary rigid registration completed.")
-                
-            resampled_moving_image_deformable = perform_deformable_bspline_registration(
-            fixed_image, moving_image, output_path, final_transform_v1, resampled_moving_image)
-            print("Deformable B-spline registration completed.")
-            # Generate checkerboard for B-Spline deformation
-            checker_image_deformable = generate_checkerboard(fixed_image, resampled_moving_image_deformable)
-            save_images(checker_image_deformable, output_path, "checkerboard_bspline_deformation")
-            # Display the checkerboard image for B-Spline deformation
-            display_images(checker_image_deformable, "Checkerboard for B-Spline deformation")
+                print("Rigid registration completed.")
+                # Generate checkerboard for Rigid Registration
+                checker_image = generate_checkerboard(fixed_image, resampled_moving_image)
+                checker_name = f"checkerboard_rigid_registration_{mask_name}"
+                save_images(checker_image, output_path, checker_name)
+                # Display the checkerboard image for Rigid Registration
+                display_images(checker_image, "Checkerboard for Rigid Registration")
 
-            # Extracting the ISO Surfaces for B-spline
-            output_resampled_image_path = os.path.join(output_path, "bspline_deformable_registration.mha")
-            output_iso_surface_file_path = os.path.join(output_path, "iso_surface_deformable.stl")
-            resampled_moving_image_deformable = sitk.ReadImage(output_resampled_image_path)
-            verts, faces = extract_iso_surface(resampled_moving_image_deformable, level=0.5, smooth=0.0)
-            save_iso_surface(verts, faces, output_iso_surface_file_path)
-
-        elif operation == 'demons':
-            print("Applying Demons algorithm...")
-            
-            output_resampled_image_path = os.path.join(output_path, "demons_registration.mha")
-            output_demons_transform_path = os.path.join(output_path, "demons_transformation.tfm")
-            
-            if os.path.exists(output_resampled_image_path) and os.path.exists(output_demons_transform_path):
-                print("Found existing Demons transformation, reading from disk.")
-                resampled_moving_image_demons = sitk.ReadImage(output_resampled_image_path)
-                demons_transform = sitk.ReadTransform(output_demons_transform_path)
-            else:
-                # Applying the Demons algorithm and saving the output
-                demons_transform, resampled_moving_image_demons = apply_demons_algorithm(fixed_image, moving_image, output_path)
-                print("Demons algorithm completed.")
-            
-                # Display the images after transformation
-                display_images(fixed_image, "Fixed Image after Demons Transformation")
-                display_images(resampled_moving_image_demons, "Resampled Moving Image after Demons Transformation")
-
-                # Generate checkerboard for Demons registration
-                checker_image_demons = generate_checkerboard(fixed_image, resampled_moving_image_demons)
-                save_images(checker_image_demons, output_path, "checkerboard_demons_registration")
-                
-                # Display the checkerboard image for Demons registration
-                display_images(checker_image_demons, "Checkerboard for Demons registration")
-
-                #
-                # Extracting the ISO Surfaces for Demons
-                output_resampled_image_path = os.path.join(output_path, "demons_registration.mha")
-                output_iso_surface_file_path = os.path.join(output_path, "iso_surface_demons.stl")
-                resampled_moving_image_demons = sitk.ReadImage(output_resampled_image_path)
-                verts, faces = extract_iso_surface(resampled_moving_image_demons, level=0.5, smooth=0.0)
+                # For ISO Surfaces for Rigid Registration
+                output_resampled_image_path = os.path.join(output_path, f"rigid_registration_{mask_name}_{idx}.mha")
+                output_iso_surface_file_path = os.path.join(output_path, f"iso_surface_rigid_{mask_name}.stl")
+                resampled_moving_image = sitk.ReadImage(output_resampled_image_path)
+                verts, faces = extract_iso_surface(resampled_moving_image, level=0.5, smooth=0.0)
                 save_iso_surface(verts, faces, output_iso_surface_file_path)
 
-        elif operation == 'dnn':
-            # Applying CNNS
-            # Use the paths passed to the function instead of hardcoding them
-            phantom_directory_path = phantom_image_path
-            patient_directory_path = patient_image_path
-            
-            print("Applying CNNS registration...")
-            transformed_moving_image = apply_dnn_registration(output_path, phantom_directory_path, patient_directory_path)
-            print("CNNS registration completed.")
-            # Generate checkerboard for CNN registration
-            checker_image_cnn = generate_checkerboard(fixed_image, transformed_moving_image)
-            save_images(checker_image_cnn, output_path, "checkerboard_cnn_registration")
-            # Display the checkerboard image for CNN registration
-            display_images(checker_image_cnn, "Checkerboard for CNN registration")
+            elif operation == 'bspline':
+                print("Performing deformable B-spline registration...")
+                # Check if final_transform_v1 and resampled_moving_image from rigid operation are available
+                rigid_transform_file = os.path.join(output_path, "rigid_transformation.tfm")
+                resampled_moving_image_file = os.path.join(output_path, "rigid_registration.mha")
+                if os.path.exists(rigid_transform_file) and os.path.exists(resampled_moving_image_file):
+                    print("Loading results from previous rigid registration...")
+                    final_transform_v1 = sitk.ReadTransform(rigid_transform_file)
+                    resampled_moving_image = sitk.ReadImage(resampled_moving_image_file)
+                else:
+                    print("Performing preliminary rigid registration...")
+                    final_transform_v1, resampled_moving_image = perform_rigid_registration(fixed_image, moving_image, output_path)
+                    print("Preliminary rigid registration completed.")
+                    
+                resampled_moving_image_deformable = perform_deformable_bspline_registration(
+                fixed_image, moving_image, output_path, final_transform_v1, resampled_moving_image)
+                print("Deformable B-spline registration completed.")
+                # Generate checkerboard for B-Spline deformation
+                checker_image_deformable = generate_checkerboard(fixed_image, resampled_moving_image_deformable)
+                checker_name = f"checkerboard_bspline_deformation_{mask_name}"
+                save_images(checker_image_deformable, output_path, checker_name)                # Display the checkerboard image for B-Spline deformation
+                display_images(checker_image_deformable, "Checkerboard for B-Spline deformation")
+
+                # Extracting the ISO Surfaces for B-spline
+                output_resampled_image_path = os.path.join(output_path, f"bspline_deformable_registration_{mask_name}_{idx}.mha")
+                output_iso_surface_file_path = os.path.join(output_path, f"iso_surface_deformable_{mask_name}.stl")
+                resampled_moving_image_deformable = sitk.ReadImage(output_resampled_image_path)
+                verts, faces = extract_iso_surface(resampled_moving_image_deformable, level=0.5, smooth=0.0)
+                save_iso_surface(verts, faces, output_iso_surface_file_path)
+
+            elif operation == 'demons':
+                print("Applying Demons algorithm...")
+                
+                output_resampled_image_path = os.path.join(output_path, "demons_registration.mha")
+                output_demons_transform_path = os.path.join(output_path, "demons_transformation.tfm")
+                
+                if os.path.exists(output_resampled_image_path) and os.path.exists(output_demons_transform_path):
+                    print("Found existing Demons transformation, reading from disk.")
+                    resampled_moving_image_demons = sitk.ReadImage(output_resampled_image_path)
+                    demons_transform = sitk.ReadTransform(output_demons_transform_path)
+                else:
+                    # Applying the Demons algorithm and saving the output
+                    demons_transform, resampled_moving_image_demons = apply_demons_algorithm(fixed_image, moving_image, output_path)
+                    print("Demons algorithm completed.")
+                
+                    # Display the images after transformation
+                    display_images(fixed_image, "Fixed Image after Demons Transformation")
+                    display_images(resampled_moving_image_demons, "Resampled Moving Image after Demons Transformation")
+
+                    # Generate checkerboard for Demons registration
+                    checker_image_demons = generate_checkerboard(fixed_image, resampled_moving_image_demons)
+                    checker_name = f"checkerboard_demons_registration_{mask_name}"
+                    save_images(checker_image_demons, output_path, checker_name)                    
+                    # Display the checkerboard image for Demons registration
+                    display_images(checker_image_demons, "Checkerboard for Demons registration")
+
+                    #
+                    # Extracting the ISO Surfaces for Demons
+                    output_resampled_image_path = os.path.join(output_path, f"demons_registration_{mask_name}_{idx}.mha")
+                    output_iso_surface_file_path = os.path.join(output_path, f"iso_surface_demons_{mask_name}.stl")
+                    resampled_moving_image_demons = sitk.ReadImage(output_resampled_image_path)
+                    verts, faces = extract_iso_surface(resampled_moving_image_demons, level=0.5, smooth=0.0)
+                    save_iso_surface(verts, faces, output_iso_surface_file_path)
+
+            elif operation == 'dnn':
+                # Applying CNNS
+                # Use the paths passed to the function instead of hardcoding them
+                phantom_directory_path = phantom_image_path
+                patient_directory_path = patient_image_path
+                
+                print("Applying CNNS registration...")
+                transformed_moving_image = apply_dnn_registration(output_path, phantom_directory_path, patient_directory_path)
+                print("CNNS registration completed.")
+                # Generate checkerboard for CNN registration
+                checker_image_cnn = generate_checkerboard(fixed_image, transformed_moving_image)
+                checker_name = f"checkerboard_cnn_registration_{mask_name}"
+                save_images(checker_image_cnn, output_path, checker_name)                # Display the checkerboard image for CNN registration
+                display_images(checker_image_cnn, "Checkerboard for CNN registration")
 
 
 if __name__ == "__main__":
