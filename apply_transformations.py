41dc6f6a6a8c6b965c9e89040ec99cafa93b57b2
Ebube Glory Ogbonda <ogbondaglory@gmail.com>
2023-11-23 12:47:52 +0100

    Update apply_transformations.py
    applying transformations to label images and then mapping these transformed labels to Hounsfield Unit (HU) values

--- apply_transformations.py
+++ apply_transformations.py
@@ -1,56 +1,70 @@
 import SimpleITK as sitk
+import numpy as np
 import os
 import sys
 
+# Parsing the .dat file to create a mapping from label values to HU values
+def parse_dat_file(dat_file_path):
+    label_to_hu = {}
+    with open(dat_file_path, 'r') as file:
+        for line in file:
+            parts = line.strip().split('\t')
+            if len(parts) >= 2:
+                label = int(parts[0])
+                hu_value = float(parts[1])
+                label_to_hu[label] = hu_value
+                print(f"Mapping label {label} to HU value {hu_value}")
+    return label_to_hu
 
-def apply_transformations_to_labels(patient_directory, labels_directory, label_filename="labels.mha"):
-    """
-    Apply specified transformations to the labels file.
+# Applying the mapping to the transformed label image
+def apply_and_map_transformation(label_image, transform_path, label_to_hu, output_path, transform_name):
+    print(f"Applying transformation: {transform_path}")
+    if not os.path.exists(transform_path):
+        print(f"Transformation file not found: {transform_path}")
+        return
 
-    Args:
-        patient_directory (str): Directory containing patient-specific transformation files.
-        labels_directory (str): Directory containing the label file.
-        label_filename (str): Filename of the label image to be transformed (default "labels.mha").
-    """
+    transform = sitk.ReadTransform(transform_path)
+    transformed_label = sitk.Resample(label_image, label_image, transform, sitk.sitkNearestNeighbor, 0.0, label_image.GetPixelID())
+
+    print(f"Mapping transformed labels to HU values for {transform_name}")
+    transformed_label_array = sitk.GetArrayFromImage(transformed_label)
+    hu_mapped_array = np.copy(transformed_label_array)
+
+    for label, hu_value in label_to_hu.items():
+        hu_mapped_array[transformed_label_array == label] = hu_value
+
+    hu_mapped_image = sitk.GetImageFromArray(hu_mapped_array)
+    hu_mapped_image.CopyInformation(transformed_label)
+
+    transformed_label_path = os.path.join(output_path, f"transformed_hu_mapped_{transform_name}.mha")
+    sitk.WriteImage(hu_mapped_image, transformed_label_path)
+    print(f"Transformed HU-mapped label image saved at {transformed_label_path}")
+
+def apply_transformations_to_labels(patient_directory, labels_directory, dat_file_path, label_filename="labels.mha"):
+    print(f"Reading label image from {labels_directory}")
     label_path = os.path.join(labels_directory, label_filename)
-    # Read the label image
     label_image = sitk.ReadImage(label_path)
 
-    # Define transformation file names
-    transformation_filenames = [
-        "rigid_transformation_lung.tfm",
-        "rigid_transformation_bone.tfm",
-        "rigid_registration.tfm"
-    ]
+    print(f"Parsing .dat file: {dat_file_path}")
+    label_to_hu = parse_dat_file(dat_file_path)
 
-    for transform_filename in transformation_filenames:
-        transform_path = os.path.join(patient_directory, transform_filename)
+    transformations = {
+        "rigid_transformation_lung": "lung",
+        "rigid_transformation_bone": "bone",
+        "rigid_registration": "full_ct"
+    }
 
-        # Check if the transformation file exists
-        if not os.path.exists(transform_path):
-            print(f"Transformation file not found: {transform_path}")
-            continue
+    for transform_file, transform_name in transformations.items():
+        transform_path = os.path.join(patient_directory, f"{transform_file}.tfm")
+        apply_and_map_transformation(label_image, transform_path, label_to_hu, patient_directory, transform_name)
 
-        # Read the transformation
-        transform = sitk.ReadTransform(transform_path)
-
-        # Apply the transformation
-        transformed_label = sitk.Resample(label_image, label_image, transform,
-                                          sitk.sitkNearestNeighbor, 0.0, label_image.GetPixelID())
-
-        # Update label_image for next transformation
-        label_image = transformed_label
-
-    # Save the final transformed label image
-    transformed_label_path = os.path.join(patient_directory, "transformed_labels.mha")
-    sitk.WriteImage(label_image, transformed_label_path)
-    print(f"Transformed label image saved at {transformed_label_path}")
-
+# Main script execution
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python apply_transformations_to_labels.py [patient_directory] [labels_directory]")
+    if len(sys.argv) < 4:
+        print("Usage: python apply_transformations_to_labels.py [patient_directory] [labels_directory] [dat_file_path]")
         sys.exit(1)
 
     patient_directory = sys.argv[1]
     labels_directory = sys.argv[2]
-    apply_transformations_to_labels(patient_directory, labels_directory)
+    dat_file_path = sys.argv[3]
+    apply_transformations_to_labels(patient_directory, labels_directory, dat_file_path)
